bd9b0b103b008f6580243f10b866b7ead32f96c9
vise1350 <vise1350@gmail.com>
2023-02-24 13:43:42 +0530

    local storage using JSON

--- main1.js
+++ main1.js
@@ -16,9 +16,22 @@
 
  }
 
- localStorage.setItem('name','vise')
- localStorage.setItem('email','vise1350@gmail.com')
- console.log(localStorage.getItem('name'))
-console.log(localStorage.getItem('email'))
+//  localStorage.setItem('name','vise')
+//  localStorage.setItem('email','vise1350@gmail.com')
+//  console.log(localStorage.getItem('name'))
+// console.log(localStorage.getItem('email'))
+let myobj={
+name:"vikram",
+age: 56
+
+};
+let myObj_serialized = JSON.stringify(myobj);
+
+localStorage.setItem("myobj",myObj_serialized)
+
+let myObj_deserialized = JSON.parse(localStorage.getItem("myobj"));
+console.log(myObj_deserialized);
+
+
 
 
